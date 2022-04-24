1b4b805f46cec8bbb8db47ebc39bd73c794b9980
Ishan Sourav <souravishan@gmail.com>
2022-04-24 16:51:20 +0530

    Added pagination

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -2,38 +2,75 @@ import React from "react";
 import { ProductContext } from "../../ProductContext";
 import { useStateValue } from "../../StateProvider";
 import ProductsListItem from "./ProductsListItem";
+import Pagination from "react-bootstrap/Pagination";
+import PageItem from "react-bootstrap/PageItem";
+
 import "./ProductsList.css";
 
 function ProductsList() {
 	const [products, setProducts] = React.useContext(ProductContext);
 	const [{ inputValue }, dispatch] = useStateValue();
+	const [activePage, setActivePage] = React.useState(1);
+	const [productsPerPage, setProductsPerPage] = React.useState(10);
+
 	let prodsViews;
 	if (inputValue === "") prodsViews = products;
 	else
 		prodsViews = products.filter((product) =>
 			product.title.toLowerCase().includes(inputValue.toLowerCase())
 		);
+
+	const indexOfLastProd = activePage * productsPerPage;
+	const indexOfFirstProd = indexOfLastProd - productsPerPage;
+	const activeProducts = prodsViews.slice(indexOfFirstProd, indexOfLastProd);
+
+	const renderProducts = activeProducts.map((product, index) => {
+		return (
+			<>
+				{activeProducts.length === 0 ? (
+					<div className="productList_notfound">
+						<h1>No Products found!!!</h1>
+					</div>
+				) : (
+					<ProductsListItem
+						title={product.title}
+						id={product.id}
+						description={product.description}
+						price={product.price}
+					/>
+				)}
+			</>
+		);
+	});
+
+	const pageNumbers = [];
+	for (
+		let number = 1;
+		number <= Math.ceil(prodsViews.length / productsPerPage);
+		number++
+	) {
+		pageNumbers.push(
+			<Pagination.Item
+				key={number}
+				active={number === activePage}
+				onClick={(e) => handleChangePage(e)}
+			>
+				{number}
+			</Pagination.Item>
+		);
+	}
+
+	const handleChangePage = (e) => {
+		console.log("e.target: ", e.target.innerHTML);
+		setActivePage(Number(e.target.innerHTML));
+	};
 	return (
-		<div className="container_productList">
-			{prodsViews.length === 0 ? (
-				<div className="productList_notfound">
-					<h1>No Products found!!!</h1>
-				</div>
-			) : (
-				prodsViews.map((product, index) => {
-					return (
-						<>
-							<ProductsListItem
-								title={product.title}
-								id={product.id}
-								description={product.description}
-								price={product.price}
-							/>
-						</>
-					);
-				})
-			)}
-		</div>
+		<>
+			<div className="container_productList">{renderProducts}</div>
+			<div>
+				<Pagination>{pageNumbers}</Pagination>
+			</div>
+		</>
 	);
 }
 
